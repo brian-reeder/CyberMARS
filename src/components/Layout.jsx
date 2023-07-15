17287fb627d581ac4cfec7a7040f46e0dba17ed9
brian-reeder <14955584+brian-reeder@users.noreply.github.com>
2023-07-15 11:50:46 -0500

    Add basePath and update favicons

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,14 +4,15 @@ import Link from 'next/link';
 import Navbar from '../components/Navbar';
 
 export default function Default({children}) {
+
 	return (
 		<>
 		  <Head>
-		    <link rel="apple-touch-icon" sizes="180x180" href="/image/apple-touch-icon.png" /> 
-		    <link rel="icon" type="image/png" sizes="32x32" href="/image/favicon-32x32.png" />
-		    <link rel="icon" type="image/png" sizes="16x16" href="/image/favicon-16x16.png" />
-			<link rel="icon" type="icon" sizes="any" href="/favicon.ico" />
-		    <link rel="manifest" href="/site.webmanifest" />
+		    <link rel="apple-touch-icon" sizes="180x180" href="/CyberMARS/image/apple-touch-icon.png" /> 
+		    <link rel="icon" type="image/png" sizes="32x32" href="/CyberMARS/image/favicon-32x32.png" />
+		    <link rel="icon" type="image/png" sizes="16x16" href="/CyberMARS/image/favicon-16x16.png" />
+			<link rel="icon" type="icon" sizes="any" href="/CyberMARS/favicon.ico" />
+		    <link rel="manifest" href="/CyberMARS/site.webmanifest" />
 		    <title>CyberMARS</title>
 		  </Head>
 		  <Navbar />
